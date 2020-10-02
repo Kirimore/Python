633246c7dcd8ed095377b1280ea57c9c4688f134
Kirimore <71873174+Kirimore@users.noreply.github.com>
2020-10-02 16:13:41 +0500

    Add files via upload

---
+++ L3-2.py
@@ -0,0 +1,10 @@
+    name = input('enter name')
+    surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+'''
+def my_func (name, surname, year, city, email, telephone):
+     return ' '.join([name, surname, year, city, email, telephone])
+print(my_func(surname = 'Menschikov', name = 'Kirill', year = '1998', city = 'Tyumen', email = 'error@mail.ru', telephone = '8-900-800-40-60'))
